9041c5c423fbdabf97db9f54cb3c7751248aa012
fahmialis <fsinergia@yahoo.com>
2021-02-26 02:11:20 +0700

    final abre bone

--- router/customer.js
+++ router/customer.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const CustomerController = require ('../controller/customerController')
-const {userLogged, testLogged }= require('../helper/loginCheck')
+const {userLogged, testLogged }= require('../middleware/loginCheck')
 
 router.get('/create', CustomerController.register)
 router.post('/store', CustomerController.store)
@@ -9,7 +9,7 @@ router.post('/store', CustomerController.store)
 router.get('/login', CustomerController.login)
 router.post('/login', CustomerController.loggedIn)
 
-// router.use(userLogged)
+router.use(userLogged)
 router.get('/logout', CustomerController.logout)
 
 router.get('/showMenu', CustomerController.showMenu)
@@ -17,7 +17,7 @@ router.post('/buy/:id', CustomerController.buyFood)
 
 
 
-// router.use(testLogged)
+router.use(testLogged)
 router.get('/list', CustomerController.showList)
 router.get('/delete/:id', CustomerController.delete)
 router.get('/foodBought/:id', CustomerController.foodHistory)
