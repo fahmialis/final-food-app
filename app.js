85ed9240a4a5125134b5754b1d6cb6544f20a654
fahmialis <fsinergia@yahoo.com>
2021-02-26 03:17:19 +0700

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const app = express()
 const router = require('./router')
-const port = 3000
+const PORT = process.env.PORT
 
 app.set('trust proxy', 1)
 app.use(session({
